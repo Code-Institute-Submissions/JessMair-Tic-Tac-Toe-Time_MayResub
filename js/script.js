c7ef3debb792c49dc5a09eea48af24a6c4c461fe
JessMair <jessiegrewal11@gmail.com>
2022-03-10 13:55:33 +0000

    Add index of board and also the winning combinations

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+
+/*
+Index of the grid
+[0] [1] [2]
+[3] [4] [5]
+[6] [7] [8]
+
+*/
+
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]  
+
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 let circleTurn
 
+//code line 8 to 16 has been added to allow the x to hover above the board for player at start of game, but does not work
 startGame()
 
 function startGame() {
@@ -12,7 +33,6 @@ function startGame() {
         cell.addEventListener('click', handleClick, {once: true})
     })
     setBoardHoverClass()
-
 }
 
 
@@ -20,6 +40,7 @@ function handleClick(e) {
     const cell = e.target 
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
+    
     //check for win
     //check for draw
 
