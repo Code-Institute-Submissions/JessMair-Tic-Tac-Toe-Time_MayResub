73dd5d61e7882b3e864914610495c15f11953f6b
JessMair <jessiegrewal11@gmail.com>
2022-05-25 13:11:20 +0000

    Remove previous html, css, and javascript to begin remaking the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //There are only two markers, an X or an O 
 
-const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle';
+
 
 /*
 Index of the grid
@@ -23,12 +22,6 @@ const winningConditions = [
 
 // The clickable cells, board game, restart button, winning message, winning message to be displayed
 
-const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('board');
-const winningMessageElement = document.getElementById('winningMessage');
-const restartButton = document.getElementById('restartButton');
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
-let circleTurn;
 
 
 // Eventlistener for when the games restart button is clicked
@@ -53,19 +46,9 @@ function startGame() {
 /* Actions to be performed when the mouse is clicked, either X or O is placed,
  the players turn swaps, the marker is also set to hover over the board until it is clicked into place */
  
-function handleClick(e) {
-    const cell = e.target;
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-    placeMark(cell, currentClass);
-    if (checkWin(currentClass)) {
-        endGame(false);
-    }  else if (isDraw()) {
-        endGame(true);
-    }  else {
-        swapTurns();
-        setBoardHoverClass();
-    } 
-} 
+function 
+
+
 
 // When the game ends, either a winner is announced or a draw in the interval message 
 
@@ -88,27 +71,19 @@ function isDraw() {
 
 // Mark placed in cell 
 
-function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass);
-}
+function placeMark
+
 
 // Function for the players to take turns 
 
 function swapTurns() {
-    circleTurn = !circleTurn;
+    
 }
 
 //Hover function
 
-function setBoardHoverClass() {
-board.classList.remove(X_CLASS);
-board.classList.remove(CIRCLE_CLASS);
-if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS);
-  } else{
-    board.classList.add(X_CLASS);
-  }
-}
+function setBoardHoverClass() 
+
 
 // Checks for winning conditions 
 
